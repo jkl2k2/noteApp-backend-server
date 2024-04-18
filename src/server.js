1932c63ca7881c4f7d372d2ee57895b9b4fd5e94
Josh <joshualomelin@gmail.com>
2024-04-18 11:00:19 -0700

    Make server running message prettier and easier to see during "npm start"

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 import app from "./app.cjs";
 
 app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+    console.log('\x1b[36m%s\x1b[0m', `
+┌────────────────────────────────┐
+│                                │
+│ Server is running on port 8080 │
+│                                │
+└────────────────────────────────┘`);
 });
